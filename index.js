113ab38d92e3c5826405dc105501c565fa555c60
Miles Higbie <miles.higbie@gmail.com>
2019-11-26 19:42:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 let musicians = [];
 let instruments = [];
  
+function theBeatlesPlay(musicians,instruments){
+  array = []; 
+  for(let i = 0; i < musicians.length; i++){
+    array.push(`${musicians} plays ${instruments}`)
+  }
+  return array
+}
